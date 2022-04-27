3607dbcb6514ec20792fde487e4024d03851f045
Sabbir131 <you@example.com>
2022-04-27 15:14:35 +0600

    Code is slightly changed and described as well

--- src/main.js
+++ src/main.js
@@ -1,38 +1,46 @@
+// Referencing  with HTML
 const notesContainer = document.getElementById("app");
 const addNoteButton = notesContainer.querySelector(".add-note");
 
+// To get every note after creating
 getNotes().forEach((note) => {
   const noteElement = createNoteElement(note.id, note.content);
   notesContainer.insertBefore(noteElement, addNoteButton);
 });
 
+// calling addNote function when it is clicked to add note
 addNoteButton.addEventListener("click", () => addNote());
 
+// To get the note 
 function getNotes() {
-  return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+  return JSON.parse(localStorage.getItem("notes-notes") || "[]");
 }
 
+// saving notes in local storage 
 function saveNotes(notes) {
-  localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+  localStorage.setItem("notes-notes", JSON.stringify(notes));
 }
 
+// creating Html elements witn js
 function createNoteElement(id, content) {
   const element = document.createElement("textarea");
 
   element.classList.add("note");
   element.value = content;
-  element.placeholder = "Empty Sticky Note";
+  element.placeholder = "Write your note";
 
+  // when any changes occur in note by user then event listener calls updateNote
   element.addEventListener("change", () => {
     updateNote(id, element.value);
   });
 
+  // eventlistener to delete a note when it is double clicked with a notice
   element.addEventListener("dblclick", () => {
-    const doDelete = confirm(
+    const doingDelete = confirm(
       "Do you want to delete it?"
     );
 
-    if (doDelete) {
+    if (doingDelete) {
       deleteNote(id, element);
     }
   });
@@ -40,6 +48,7 @@ function createNoteElement(id, content) {
   return element;
 }
 
+// Add note function
 function addNote() {
   const notes = getNotes();
   const noteObject = {
@@ -54,6 +63,7 @@ function addNote() {
   saveNotes(notes);
 }
 
+// Update note function 
 function updateNote(id, newContent) {
   const notes = getNotes();
   const targetNote = notes.filter((note) => note.id == id)[0];
@@ -62,6 +72,7 @@ function updateNote(id, newContent) {
   saveNotes(notes);
 }
 
+// To delete a note
 function deleteNote(id, element) {
   const notes = getNotes().filter((note) => note.id != id);
 
